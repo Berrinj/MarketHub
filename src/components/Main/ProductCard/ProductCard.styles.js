d47f66634610cff6a1d1207d9bbe50be774dffce
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-11-12 12:12:40 +0100

    dev setup complete

---
+++ src/components/Main/ProductCard/ProductCard.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 16px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 16px;
+`;
