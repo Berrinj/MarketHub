69eab156af82a525dd9a4aaf676e20cac04a8a13
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-11-14 10:14:12 +0100

    created single product fetch

--- src/api/fetch.jsx
+++ src/api/fetch.jsx
@@ -2,7 +2,7 @@ const BASE_URL = 'https://v2.api.noroff.dev/online-shop';
 
 export async function FetchProducts() {
     try {
-    const response = await fetch(`${BASE_URL}`);
+    const response = await fetch(BASE_URL);
     const data = await response.json();
     console.log("response", data);
     return data;
@@ -10,3 +10,14 @@ export async function FetchProducts() {
     console.error("Ran into a problem fetching products:", error);
 }
     }
+
+export async function FetchProductById(id) {
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`);
+        const data = await response.json();
+        console.log("response", data);
+        return data;
+    } catch (error) {
+        console.error("Ran into a problem fetching product by id:", error);
+    }
+}
